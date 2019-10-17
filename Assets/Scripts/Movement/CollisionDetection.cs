22527233197fdcd941c7785a4528b6c450a5b599
Mattias Aksli <mattias.aksli@gmail.com>
2019-10-17 20:09:45 +0300

    Implemented basic camera movement and border system

--- Assets/Scripts/Movement/CollisionDetection.cs
+++ Assets/Scripts/Movement/CollisionDetection.cs
@@ -1,13 +1,12 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class CollisionDetection : MonoBehaviour
 {
     private BoxCollider2D boxCollider2D;
     public static CollisionDetection coll;
-    private List<RaycastHit2D> hitBufferList = new List<RaycastHit2D> (10);
-    
+    private List<RaycastHit2D> hitBufferList = new List<RaycastHit2D>(10);
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,7 +17,7 @@ public class CollisionDetection : MonoBehaviour
     public void Move(KinematicBody body, Vector2 add)
     {
         RaycastHit2D[] results = new RaycastHit2D[10];
-        List<RaycastHit2D> resultsList = new List<RaycastHit2D> (10);
+        List<RaycastHit2D> resultsList = new List<RaycastHit2D>(10);
         int n;
         float padding = 0.002f;
 
@@ -26,7 +25,7 @@ public class CollisionDetection : MonoBehaviour
         Vector2 xcomp = new Vector2(add.x, 0);
         n = body.boxCollider.Cast(xcomp, results);
         resultsList.Clear();
-        for(var i = 0; i < n; i++)
+        for (var i = 0; i < n; i++)
         {
             resultsList.Add(results[i]);
         }
