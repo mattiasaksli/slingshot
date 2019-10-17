6444670bf3b81a6f3ab0134ca4f939fde5ffab30
TanelMarran <marran.tanel@gmail.com>
2019-10-17 17:28:26 +0300

    Fixed error

---
+++ Assets/Scripts/SpriteClamp.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteClamp : MonoBehaviour
+{
+    private SpriteRenderer sprite;
+    public float pixelperUnit = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sprite = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 parentpos = transform.parent.transform.position;
+        parentpos.x = Mathf.Round(parentpos.x / pixelperUnit) * pixelperUnit;
+        parentpos.y = Mathf.Round(parentpos.y / pixelperUnit) * pixelperUnit;
+        transform.position = parentpos;
+    }
+}
