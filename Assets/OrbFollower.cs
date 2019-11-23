163cc9590ae3053d2479d2bb227fb1d1a95d8845
TanelMarran <marran.tanel@gmail.com>
2019-11-23 20:13:44 +0200

    Started working on orb availability indicator

---
+++ Assets/OrbFollower.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbFollower : MonoBehaviour
+{
+    public Transform Orb;
+    public Vector3 TargetPosition;
+    public bool Available;
+
+    private PlayerController player;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = gameObject.GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
