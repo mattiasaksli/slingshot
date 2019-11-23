87c4c27988bc3e110f299ac57a977fe851c8867c
TanelMarran <marran.tanel@gmail.com>
2019-11-23 20:52:52 +0200

    Added indicator for when you have a slingshot charge available

--- Assets/OrbFollower.cs
+++ Assets/OrbFollower.cs
@@ -7,18 +7,29 @@ public class OrbFollower : MonoBehaviour
     public Transform Orb;
     public Vector3 TargetPosition;
     public bool Available;
+    public float LerpSpeed;
 
     private PlayerController player;
+    private SpriteRenderer sprite;
     
     // Start is called before the first frame update
     void Start()
     {
         player = gameObject.GetComponent<PlayerController>();
+        sprite = Orb.gameObject.GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Orb.position = Vector3.Lerp(Orb.position, TargetPosition, LerpSpeed*Time.deltaTime);
+        if(player.orb)
+        {
+            TargetPosition = player.orb.transform.position;
+        } else
+        {
+            TargetPosition = player.transform.position + new Vector3((player.IsFacingRight ? -1 : 1),0,0);
+        }
+        sprite.enabled = player.IsOrbAvailable;
     }
 }
