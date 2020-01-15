658ddcf9742b70335a1916a0406e65c8e24e2952
Mattias Aksli <mattiasaksli@hotmail.com>
2020-01-15 18:09:16 +0200

    Added resolutions dropdown.

--- Assets/Scripts/SettingsScript.cs
+++ Assets/Scripts/SettingsScript.cs
@@ -1,9 +1,37 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
 using UnityEngine.Audio;
 
 public class SettingsScript : MonoBehaviour
 {
     public AudioMixer audioMixer;
+    public Resolution[] resolutions;
+    public TMP_Dropdown resolutionDropdown;
+
+    private void Start()
+    {
+        QualitySettings.vSyncCount = 0;
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+
+        int currentResIndex = 0;
+        List<string> options = new List<string>();
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height + " @ " + resolutions[i].refreshRate + "HZ";
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.width && resolutions[i].height == Screen.height)
+            {
+                currentResIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
 
     public void SetMasterVolume(float volume)
     {
@@ -30,4 +58,10 @@ public class SettingsScript : MonoBehaviour
         Screen.fullScreen = isFullscreen;
     }
 
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution res = resolutions[resolutionIndex];
+        Screen.SetResolution(res.width, res.height, Screen.fullScreen, res.refreshRate);
+        Application.targetFrameRate = res.refreshRate;
+    }
 }
