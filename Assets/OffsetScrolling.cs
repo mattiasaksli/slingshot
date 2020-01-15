e83310241e7f06d73a2c82cf3332988b25716bef
TanelMarran <marran.tanel@gmail.com>
2020-01-15 17:09:37 +0200

    Working on parallax background

---
+++ Assets/OffsetScrolling.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OffsetScrolling : MonoBehaviour
+{
+    // Scroll main texture based on time
+
+    float scrollSpeed = 0.5f;
+    Renderer rend;
+    public Vector2 scrolling;
+    public Vector2 ScrollingSpeed;
+    Camera camera;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        camera = Camera.main;
+    }
+
+    void FixedUpdate()
+    {
+        /*scrolling = camera.transform.position;
+        scrolling.x *= ScrollingSpeed.x;
+        scrolling.y *= ScrollingSpeed.y;*/
+        rend.material.SetVector("_Scrolling", scrolling);
+    }
+}
