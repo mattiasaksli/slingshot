246b69170cc335d43a4055ca17aa9e65fd603a80
Mattias Aksli <mattiasaksli@hotmail.com>
2020-01-26 17:00:30 +0200

    Options persist after loading scenes or exiting the game.

--- Assets/Scripts/SettingsScript.cs
+++ Assets/Scripts/SettingsScript.cs
@@ -2,6 +2,7 @@
 using TMPro;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SettingsScript : MonoBehaviour
 {
@@ -9,17 +10,55 @@ public class SettingsScript : MonoBehaviour
     public Resolution[] resolutions;
     public TMP_Dropdown resolutionDropdown;
     public AudioSource sfx;
+    public Slider MasterSlider;
+    public Slider UISlider;
+    public Slider GameSlider;
+    public Slider BGMSlider;
+    public Toggle FullscreenToggle;
 
     private Dictionary<int, int> actualResolutionIndex = new Dictionary<int, int>();
     // there is a difference between the indexes of the resolutions array and the resolutions dropdown because of the refresh rates
 
-    private void Start()
+    private void Awake()
     {
+        Debug.Log("Reading start");
+
+        #region Read from prefs
+        float volume = PlayerPrefs.GetFloat("MasterVolume", 0);
+        audioMixer.SetFloat("MasterVolume", volume);
+        MasterSlider.value = Mathf.Pow(10f, volume / 20f);
+
+        volume = PlayerPrefs.GetFloat("UISFXVolume", 0);
+        audioMixer.SetFloat("UISFXVolume", volume);
+        UISlider.value = Mathf.Pow(10f, volume / 20f);
+
+        volume = PlayerPrefs.GetFloat("GameSFXVolume", 0);
+        audioMixer.SetFloat("GameSFXVolume", volume);
+        GameSlider.value = Mathf.Pow(10f, volume / 20f);
+
+        volume = PlayerPrefs.GetFloat("BGMVolume", 0);
+        audioMixer.SetFloat("BGMVolume", volume);
+        BGMSlider.value = Mathf.Pow(10f, volume / 20f);
+
+        Screen.fullScreen = PlayerPrefs.GetInt("isFullscreen", 0) == 1;
+        FullscreenToggle.isOn = Screen.fullScreen;
+
+        int currentWidth = PlayerPrefs.GetInt("ResolutionW", 800);
+        Debug.Log(currentWidth);
+        int currentHeight = PlayerPrefs.GetInt("ResolutionH", 600);
+        Debug.Log(currentHeight);
+        int currentRefreshRate = PlayerPrefs.GetInt("ResolutionR", 59);
+        Debug.Log(currentRefreshRate);
+        Screen.SetResolution(currentWidth, currentHeight, Screen.fullScreen, currentRefreshRate);
+        Debug.Log(Screen.width + " " + Screen.height);
+
+        int currentResIndex = PlayerPrefs.GetInt("ResolutionDropdownIndex", -1);
+        #endregion
+
         QualitySettings.vSyncCount = 0;
         resolutions = Screen.resolutions;
         resolutionDropdown.ClearOptions();
 
-        int currentResIndex = 0;
         List<string> options = new List<string>();
         int maxRefreshRate = 0;
         for (int i = 0; i < resolutions.Length; i++)
@@ -36,7 +75,7 @@ public class SettingsScript : MonoBehaviour
                 actualResolutionIndex.Add(options.Count - 1, i);
             }
 
-            if (resolutions[i].width == Screen.width && resolutions[i].height == Screen.height)
+            if (resolutions[i].width == Screen.width && resolutions[i].height == Screen.height && currentResIndex == -1)
             {
                 currentResIndex = i;
             }
@@ -52,7 +91,8 @@ public class SettingsScript : MonoBehaviour
         if (sfx.mute) { sfx.mute = false; }
         audioMixer.SetFloat("MasterVolume", Mathf.Log10(volume) * 20);
         sfx.outputAudioMixerGroup = audioMixer.FindMatchingGroups("Master")[0];
-        if (!sfx.isPlaying && Time.time > 1) { sfx.Play(); }    //Time.time > 1 because otherwise it will play the sfx at startup.
+        if (!sfx.isPlaying && Time.timeSinceLevelLoad > 1) { sfx.Play(); }
+        //Time.timeSinceLevelLoad > 1 because otherwise it will play the sfx at startup.
     }
 
     public void SetUISFXVolume(float volume)
@@ -60,7 +100,7 @@ public class SettingsScript : MonoBehaviour
         if (sfx.mute) { sfx.mute = false; }
         audioMixer.SetFloat("UISFXVolume", Mathf.Log10(volume) * 20);
         sfx.outputAudioMixerGroup = audioMixer.FindMatchingGroups("UI")[0];
-        if (!sfx.isPlaying && Time.time > 1) { sfx.Play(); }
+        if (!sfx.isPlaying && Time.timeSinceLevelLoad > 1) { sfx.Play(); }
     }
 
     public void SetGameSFXVolume(float volume)
@@ -68,7 +108,7 @@ public class SettingsScript : MonoBehaviour
         if (sfx.mute) { sfx.mute = false; }
         audioMixer.SetFloat("GameSFXVolume", Mathf.Log10(volume) * 20);
         sfx.outputAudioMixerGroup = audioMixer.FindMatchingGroups("Game")[0];
-        if (!sfx.isPlaying && Time.time > 1) { sfx.Play(); }
+        if (!sfx.isPlaying && Time.timeSinceLevelLoad > 1) { sfx.Play(); }
     }
 
     public void SetBGMVolume(float volume)
@@ -86,9 +126,33 @@ public class SettingsScript : MonoBehaviour
         QualitySettings.SetQualityLevel(qualityIndex);
     }
 
+
     public void SetResolution(int resolutionIndex)
     {
         Resolution res = resolutions[actualResolutionIndex[resolutionIndex]];
         Screen.SetResolution(res.width, res.height, Screen.fullScreen, res.refreshRate);
+
+        PlayerPrefs.SetInt("ResolutionW", res.width);
+        PlayerPrefs.SetInt("ResolutionH", res.height);
+        PlayerPrefs.SetInt("ResolutionR", res.refreshRate);
+        PlayerPrefs.SetInt("ResolutionDropdownIndex", resolutionIndex);
+    }
+
+    private void OnDisable()
+    {
+        audioMixer.GetFloat("MasterVolume", out float masterVolume);
+        PlayerPrefs.SetFloat("MasterVolume", masterVolume);
+
+        audioMixer.GetFloat("UISFXVolume", out float uisfxVolume);
+        PlayerPrefs.SetFloat("UISFXVolume", uisfxVolume);
+
+        audioMixer.GetFloat("GameSFXVolume", out float gameVolume);
+        PlayerPrefs.SetFloat("GameSFXVolume", gameVolume);
+
+        audioMixer.GetFloat("BGMVolume", out float bgmVolume);
+        PlayerPrefs.SetFloat("BGMVolume", bgmVolume);
+
+        int fullscreen = Screen.fullScreen ? 1 : 0;
+        PlayerPrefs.SetInt("isFullscreen", fullscreen);
     }
 }
